85eb647c6934a33abf11e5439095654106f1c9c3
olexiygolovko <olexiygolvoko92@mai.ru>
2023-03-19 15:31:40 +0300

    added DELETE method, added styles

--- publicposts/publicposts/urls.py
+++ publicposts/publicposts/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path,include, re_path
 from publicpostsapp import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^api/posts', views.posts),
-    re_path(r'^api/like_post/(?P<post_id>[0-9]+)$', views.like_post)
+    re_path(r'^api/like_post/(?P<post_id>[0-9]+)$', views.like_post),
+    re_path(r'^api/delete_post/(?P<post_id>[0-9]+)$', views.delete_post),
 ]
 
--- publicposts/publicpostsapp/views.py
+++ publicposts/publicpostsapp/views.py
@@ -7,17 +7,30 @@ from rest_framework import status
 from .models import Post
 from .serializers import *
 
-@api_view(['GET', 'POST'])
+@api_view(['GET', 'POST', 'DELETE'])
 def posts(request):
-   if request.method == 'GET':
-    posts = Post.objects.all()
-    serializer = PostSerializer(posts, many = True)
-    return(Response({'data': serializer.data}))
-   elif request.method == 'POST':
-    post = Post()
-    post.text = request.data['text']
-    post.save()
-    return Response(status=status.HTTP_200_OK)
+    if request.method == 'GET':
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response({'data': serializer.data})
+
+    elif request.method == 'POST':
+        post = Post()
+        post.text = request.data['text']
+        post.save()
+        return Response(status=status.HTTP_201_CREATED)
+
+@api_view(['DELETE'])
+def delete_post(request, post_id):
+    try:
+        post = Post.objects.get(id=post_id)
+        post.delete()
+        return Response({'message': 'Post has been deleted'}, status=status.HTTP_204_NO_CONTENT)
+    except Post.DoesNotExist:
+        return Response({'error': 'Post not found'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
 
 @api_view(['GET'])
 def like_post(request, post_id):
